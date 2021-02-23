26ad03610a33d63e9d35e0a3ac4eff94ab8efe17
Krissada Sumalai <krissada.sumalai@gmail.com>
2021-02-23 23:48:25 +0700

    Updates

---
+++ precache-manifest.726ca06b2b5b6e4bafa29a5861877696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82df6b30f2d52ddde69a9f0e9763983f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b65b6655c89a995881d5",
+    "url": "./static/css/main.9e24c1c7.chunk.css"
+  },
+  {
+    "revision": "908dd6ea3b0512c336fe",
+    "url": "./static/js/2.e36fe803.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e36fe803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65b6655c89a995881d5",
+    "url": "./static/js/main.d4495ffa.chunk.js"
+  },
+  {
+    "revision": "749f65d95385c65c0bd0",
+    "url": "./static/js/runtime-main.c8b1fad5.js"
+  }
+]);
