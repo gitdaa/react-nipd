5d12a78c3c85023c4929e0456dbaf4b451146c5d
gitda <krissada.sumalai@gmail.com>
2021-08-05 00:07:12 +0700

    add ward group_name

--- src/shared/types/lookup/ward.tsx
+++ src/shared/types/lookup/ward.tsx
@@ -1,13 +1,14 @@
 export interface Ward {
-  ward: number
-  name: string
-  shortname: string
+  ward: number;
+  name: string;
+  shortname: string;
+  group_name: string | null;
 }
 
 export interface LookupWardRequest {
-  isFetching: boolean
-  data: Ward[]
-  error: boolean
-  errorMessage: string | null
-  expireAt: Date | null
+  isFetching: boolean;
+  data: Ward[];
+  error: boolean;
+  errorMessage: string | null;
+  expireAt: Date | null;
 }
