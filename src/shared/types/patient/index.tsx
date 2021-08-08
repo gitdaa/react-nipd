2ad67c7fd10279d1f018db7b6a27ec73ddde7d89
gitda <krissada.sumalai@gmail.com>
2021-08-08 21:45:03 +0700

    add hometel

--- src/shared/types/patient/index.tsx
+++ src/shared/types/patient/index.tsx
@@ -1,102 +1,103 @@
 /* eslint-disable camelcase */
-import { BaseRequest } from '..'
-import { FlashNotifyItem } from '../notify'
-import { VitalSignItem, VitalSignNoteItem } from '../nursing/vitalsign'
+import { BaseRequest } from '..';
+import { FlashNotifyItem } from '../notify';
+import { VitalSignNoteItem } from '../nursing/vitalsign';
 
 export interface Patient {
-  hn: string
-  an: number
-  vn: number
-  bed: string
-  sex: number
-  income: number
-  wait_paid: number
-  name: string
-  ward_code: number
-  ward: number
-  date: number
-  age: number
-  avatar: string
-  diag_group_code?: number
-  diag_items_code?: number
+  hn: string;
+  an: number;
+  vn: number;
+  bed: string;
+  sex: number;
+  income: number;
+  wait_paid: number;
+  name: string;
+  ward_code: number;
+  ward: number;
+  date: number;
+  age: number;
+  avatar: string;
+  diag_group_code?: number;
+  diag_items_code?: number;
+  hometel?: string;
 
-  notify_count?: number
-  notify?: FlashNotifyItem[]
-  notifys?: PatientNotifyGroup
-  vitalsign: VitalSignNoteItem[]
+  notify_count?: number;
+  notify?: FlashNotifyItem[];
+  notifys?: PatientNotifyGroup;
+  vitalsign: VitalSignNoteItem[];
 }
 
 export interface PatientNotifyGroup {
-  laboratory?: FlashNotifyItem[]
-  medication?: FlashNotifyItem[]
-  allergy?: FlashNotifyItem[]
-  xray?: FlashNotifyItem[]
-  vitalsign?: FlashNotifyItem[]
+  laboratory?: FlashNotifyItem[];
+  medication?: FlashNotifyItem[];
+  allergy?: FlashNotifyItem[];
+  xray?: FlashNotifyItem[];
+  vitalsign?: FlashNotifyItem[];
 }
 
 export interface PatientInfo extends Patient {
-  birthday: string
-  tel: string
-  address: string
-  pttype: string
-  regdate: string
-  regtime: string
-  dchdate: string
-  dchtime: string
-  cc: string
-  prediag: string
-  doctor: string
-  access_key: string
+  birthday: string;
+  tel: string;
+  address: string;
+  pttype: string;
+  regdate: string;
+  regtime: string;
+  dchdate: string;
+  dchtime: string;
+  cc: string;
+  prediag: string;
+  doctor: string;
+  access_key: string;
 }
 
 export interface PatientState {
-  expireAt: string | null
-  isFetching: boolean
-  error: boolean
-  errorMessage: string | null
-  hasPatient: boolean
-  patient: PatientInfo | undefined
-  isIpd: boolean
+  expireAt: string | null;
+  isFetching: boolean;
+  error: boolean;
+  errorMessage: string | null;
+  hasPatient: boolean;
+  patient: PatientInfo | undefined;
+  isIpd: boolean;
 }
 
 export interface PatientSearchState {
-  isFetching: boolean
-  data: Patient[]
-  error: boolean
+  isFetching: boolean;
+  data: Patient[];
+  error: boolean;
 }
 
 export interface PatientInfoAllergies {
-  agent: string
-  report_date: string
-  symptom: string
+  agent: string;
+  report_date: string;
+  symptom: string;
 }
 
 export interface PatientOwnerRequest extends BaseRequest<Patient[]> {
-  errorAttempt: number
-  filter: string | null
+  errorAttempt: number;
+  filter: string | null;
 }
 
 export type PatientStanding = PatientInfo & {
-  standing_id: string
-  standing_date: number
-  standing_ward: number
+  standing_id: string;
+  standing_date: number;
+  standing_ward: number;
 
-  nward?: number | null
-  oward?: number | null
-  nward_name?: string | null
-  oward_name?: string | null
-}
+  nward?: number | null;
+  oward?: number | null;
+  nward_name?: string | null;
+  oward_name?: string | null;
+};
 
 export interface PatientStandingRequest extends BaseRequest<PatientStanding[]> {
-  filter: string | null
+  filter: string | null;
 }
 
 export interface HealthInfo {
-  an: number
-  bw: number
-  height: number
-  bmi: number
-  clinic: string
+  an: number;
+  bw: number;
+  height: number;
+  bmi: number;
+  clinic: string;
 }
 
 export interface PatientHealthInfoRequest extends BaseRequest<HealthInfo[]> {}
@@ -113,54 +114,54 @@ export interface PatientHealthVitalSignRequest
 // 'respiratory_rate' => $respiratory_rate,
 
 export interface HealthTemperature {
-  nurse_note_id: number
-  an: number
-  note_date: string
-  note_time: string
-  unit: string
-  temperature: number
+  nurse_note_id: number;
+  an: number;
+  note_date: string;
+  note_time: string;
+  unit: string;
+  temperature: number;
 }
 
 export interface HealthBloodPressure {
-  nurse_note_id: number
-  an: number
-  note_date: string
-  note_time: string
-  unit: string
-  bp_systolic: number
-  bp_diastolic: number
+  nurse_note_id: number;
+  an: number;
+  note_date: string;
+  note_time: string;
+  unit: string;
+  bp_systolic: number;
+  bp_diastolic: number;
 }
 
 export interface HealthPulse {
-  nurse_note_id: number
-  an: number
-  note_date: string
-  note_time: string
-  unit: string
-  pulse: number
+  nurse_note_id: number;
+  an: number;
+  note_date: string;
+  note_time: string;
+  unit: string;
+  pulse: number;
 }
 
 export interface HealthHeartRate {
-  nurse_note_id: number
-  an: number
-  note_date: string
-  note_time: string
-  unit: string
-  heart_rate: number
+  nurse_note_id: number;
+  an: number;
+  note_date: string;
+  note_time: string;
+  unit: string;
+  heart_rate: number;
 }
 export interface HealthRespriratoryRate {
-  nurse_note_id: number
-  an: number
-  note_date: string
-  note_time: string
-  unit: string
-  respiratory_rate: number
+  nurse_note_id: number;
+  an: number;
+  note_date: string;
+  note_time: string;
+  unit: string;
+  respiratory_rate: number;
 }
 
 export interface HealthVitalSignRecent {
-  temperature: undefined | HealthTemperature[]
-  blood_pressure: undefined | HealthBloodPressure[]
-  pulse: undefined | HealthPulse[]
-  heart_rate: undefined | HealthHeartRate[]
-  respiratory_rate: undefined | HealthRespriratoryRate[]
+  temperature: undefined | HealthTemperature[];
+  blood_pressure: undefined | HealthBloodPressure[];
+  pulse: undefined | HealthPulse[];
+  heart_rate: undefined | HealthHeartRate[];
+  respiratory_rate: undefined | HealthRespriratoryRate[];
 }
